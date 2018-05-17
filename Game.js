42cf5eb3cb06bb5213e6369e067f7799cd9c6c4c
Henryk Krzyszczak <h.krzyszczak@electriclight.pl>
2018-05-17 22:36:13 +0200

    first commit

---
+++ Game.js
@@ -0,0 +1,17 @@
+function Game() {
+
+}
+
+Game.prototype.init = function() {
+    this.gameWorld = new GameWorld();
+}
+
+Game.prototype.start = function() {
+    
+}
+
+Game.prototype.mainLoop = function() {
+
+}
+
+let PoolGame = new Game();
